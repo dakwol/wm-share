d632859ab55f18cedcd80387efedd288c269dad2
Dakwol <daniilfomenko99@gmail.com>
2022-03-19 23:45:24 +0300

    create pin

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { client } from '../client';
@@ -7,35 +7,40 @@ import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
-  const[pins, setPins] = useState(null);
-  const {categoryId} = useParams();
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
 
   useEffect(() => {
-    setLoading(true);
-    if(categoryId){
+    if (categoryId) {
+      setLoading(true);
       const query = searchQuery(categoryId);
-
-      client.fetch(query)
-        .then((data) => {
-          setPins(data);
-          setLoading(false);
-        })
-    } else {
-      client.fetch(feedQuery)
-      .then((data) => {
+      client.fetch(query).then((data) => {
         setPins(data);
         setLoading(false);
-      })
-    }
-  }, [categoryId])
+      });
+    } else {
+      setLoading(true);
 
-  if(loading) return <Spinner message="Мы добавляем новые мифы в вашу ленту!" />
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || 'new';
+  if (loading) {
+    return (
+      <Spinner message={`Мы добавляем ${ideaName} мифы в вашу ленту!`} />
+    );
+  }
   return (
     <div>
-      {pins && <MasonryLayout pins={pins}/>}
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
